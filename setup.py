b40ec0a90a4449a0c2db4e8beaaca0a78bec493a
sinwoobang <sinwoobang@gmail.com>
2020-05-03 19:45:44 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+setup(
+    name='django-michaeljackson',
+    version='0.0.1',
+    license='BSD',
+    description='Web APIs for Django, made easy.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Sin-Woo Bang',
+    author_email='sinwoobang@gmail.com',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    install_requires=["django>=2.2"],
+    python_requires=">=3.5",
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    project_urls={
+        'Source': 'https://github.com/sinwoobang/django-michaeljackson',
+    },
+)
