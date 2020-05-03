82715547e4593ddadd44bf77e1f90012bdcb8172
sinwoobang <sinwoobang@gmail.com>
2020-05-03 19:29:15 +0900

    Implement __call__ in MJMiddleware

--- michaeljackson/middleware.py
+++ michaeljackson/middleware.py
@@ -1,10 +1,11 @@
-from django_michaeljackson.data import get_random_info
+from michaeljackson.data import get_random_info
 
 
 class MJMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def process_response(self, request, response):
+    def __call__(self, request):
+        response = self.get_response(request)
         response['X-Michael-Jackson'] = get_random_info()
         return response
